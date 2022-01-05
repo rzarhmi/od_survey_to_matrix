f27192c9e4c63b21885925ba72d22f21de2b5316
rzarhmi <rezarahimiazghan@gmail.com>
2022-01-06 00:41:15 +0330

    solve some problems

--- RefineODs.py
+++ RefineODs.py
@@ -12,7 +12,7 @@ def refine_ods(od,
     od = od.astype(int)
     print("Data Trimmed successfully")
     print("Starting to refine it even further...")
-    od = od[od["d_mode1"].isin([1, 2, 3, 4, 5, 6, 7, 13, 14])]
+    od = od[od["d_mode1"].isin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])]
     origins = list(set(list(od[orig_column])))
     origins.sort()
     destinations = list(set(list(od[dest_column])))
@@ -21,11 +21,12 @@ def refine_ods(od,
     for i, loc in enumerate(origins):
         location_map[loc] = i + 1
     mode_map = {1: "Vehicle", 2: "Vehicle", 3: "Bus", 4: "Subway", 5: "Bus",
-               6: "Bus", 7: "Bus", 13: "Bike", 14: "Walk"}
+                6: "Bus", 7: "Bus",8: "Train", 9: "Bus", 10: "Bus", 11: "Taxi",
+                12: "Motorcycle", 13: "Bike", 14: "Walk"}
     refined_od = od[od[time_column]<=2400]
     refined_od.replace({mode_column: mode_map}, inplace=True)
     refined_od.replace({orig_column: location_map}, inplace=True)
     refined_od.replace({dest_column: location_map}, inplace=True)
     refined_od[time_column] = refined_od[time_column] // 100
-    print("DONE")
+    print("DONE REFINING")
     return refined_od
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from RefineODs import refine_ods
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file_name', '-fn', help="Name of the OD survey file", required=True)
-parser.add_argument('--time', '-t',type=int, help="Start time of the OD Matrix", required=True)
-parser.add_argument('--state', '-s', default="separated",help="Can be either 'separated' or 'together' to decide on whether to divide matrices to different modes of transportation (default together)")
-parser.add_argument('--mode', '-m', default="Vehicle",help="If the state is chosen to be separate, a mode should be specified from ['Vehicle', 'Bus', 'Subway', 'Walk/Bike']")
+parser.add_argument('--time', '-t', default="all", help="Start time of the OD Matrix")
+parser.add_argument('--mode', '-m', default="all", help="If the state is chosen to be separate, a mode should be specified from ['Vehicle', 'Bus', 'Subway', 'Walk/Bike']")
 args = parser.parse_args()
 
 
 class ProcessODMatrix:
-    def __init__(self, already_refined, file_name, time, state, mode, orig_column='d_orisr', dest_column='d_dessr',
+    def __init__(self, already_refined, file_name, time, mode, orig_column='d_orisr', dest_column='d_dessr',
                  time_column='d_hrede', mode_column='d_mode1'):
         self.survey = pd.read_csv(file_name)
         if not already_refined:
@@ -21,11 +21,8 @@ class ProcessODMatrix:
         self.time_column = time_column
         self.mode_column = mode_column
         self.time = time
-        if state == "separated":
-            self.mode = mode
-            self.get_matrix_for_time_mode()
-        else:
-            self.get_matrix_for_time()
+        self.mode = mode
+        self.get_matrix()
 
     def make_file(self, info_dict, time, mode="all"):
         result = "$OR,D2\n* From-Time To-Time\n"
@@ -45,7 +42,9 @@ class ProcessODMatrix:
         od_matrix_file_name = "OD_MATRIX_" + str(time) + "-" + str(time + 1)
         if mode != "all":
             od_matrix_file_name += "_" + mode
-        od_matrix_file = open("OD Matrix/" + od_matrix_file_name + ".od", "w")
+        if not os.path.exists("OD Matrix"):
+            os.makedirs("OD Matrix")
+        od_matrix_file = open("OD Matrix/" + od_matrix_file_name + ".od", "w+")
         n = od_matrix_file.write(result)
         od_matrix_file.close()
 
@@ -58,7 +57,32 @@ class ProcessODMatrix:
         survey_grouped = survey.groupby([orig_column, dest_column]).size()
         survey_dict = dict(survey_grouped)
         self.make_file(survey_dict, time, mode)
+        
+    def get_matrix(self):
+        time_column, mode_column = self.time_column, self.mode_column
+        orig_column, dest_column = self.orig_column, self.dest_column
+        time, mode, survey = self.time, self.mode, self.survey
+        print(mode)
+        if time != "all":
+            survey = survey[(survey[time_column] == time)]
+        if mode != "all":
+            print(len(survey))
+            survey = survey[(survey[mode_column] == mode)]
+            print(len(survey))
 
+        if time == "all":
+            for time_index in range(0, 25):
+                temp_survey = survey[(survey[time_column] == time_index)]
+                print("FOR time " + str(time_index) + " " + str(len(temp_survey )))
+                survey_grouped = temp_survey .groupby([orig_column, dest_column]).size()
+                survey_dict = dict(survey_grouped)
+                self.make_file(survey_dict, time_index, mode)
+        else:
+            survey_grouped = survey.groupby([orig_column, dest_column]).size()
+            survey_dict = dict(survey_grouped)
+            self.make_file(survey_dict, time, mode)
+
+        
     def get_matrix_for_time(self):
         time_column, mode_column = self.time_column, self.mode_column
         orig_column, dest_column = self.orig_column, self.dest_column
@@ -73,15 +97,24 @@ class ProcessODMatrix:
 if __name__=="__main__":
     SURVEY_FILE_NAME = args.file_name
     TIME = args.time
-    STATE = args.state
-    if STATE == "separated":
-        MODE = args.mode
+    TIME = TIME if TIME == "all" else int(TIME)
+    MODE = args.mode
+    MODE_STATE = "all" if MODE=="all" else "separated"
+    TIME_STATE = "all" if TIME=="all" else "separated"
+    OK = True
+    if MODE not in ["all", "Vehicle", "Bus", "Subway", "Taxi", "Walk", "Bike", "Motorcycle", "Train"]:
+        print("Please enter a valid mode of transportation between "+str(["all", "Vehicle", "Bus", "Subway", "Taxi", "Walk", "Bike", "Motorcycle", "Train"]))
+        OK = False
+    if TIME not in ["all"] + list(range(0, 24)):
+        print("Please enter a valid time between 0 and 24 or enter all")
+        OK = False
+
 
     ORIG_COLUMN_NAME = 'd_orisr'
     DEST_COLUMN_NAME = 'd_dessr'
     TIME_COLUMN_NAME = 'd_hrede'
     MODE_COLUMN_NAME = 'd_mode1'
-    process_OD_matrix = ProcessODMatrix(already_refined=False, file_name=SURVEY_FILE_NAME,
-                                        time=TIME, state=STATE, mode=MODE,
-                                        orig_column=ORIG_COLUMN_NAME, dest_column=DEST_COLUMN_NAME,
-                                        time_column=TIME_COLUMN_NAME, mode_column=MODE_COLUMN_NAME)
+    if OK :
+        process_OD_matrix = ProcessODMatrix(already_refined="refined" in SURVEY_FILE_NAME, file_name=SURVEY_FILE_NAME,
+                                            time=TIME, mode=MODE,orig_column=ORIG_COLUMN_NAME, dest_column=DEST_COLUMN_NAME,
+                                            time_column=TIME_COLUMN_NAME, mode_column=MODE_COLUMN_NAME)
